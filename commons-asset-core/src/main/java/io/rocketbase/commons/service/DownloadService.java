ef96293f396425a90d778142b8a2ca2605f5f1a2
Marten Prieß <marten@rocketbase.io>
2018-07-25 14:45:48 +0200

    fallback in case of typeDetection not possible from filename, tika will check afterwards again...

--- commons-asset-core/src/main/java/io/rocketbase/commons/service/DownloadService.java
+++ commons-asset-core/src/main/java/io/rocketbase/commons/service/DownloadService.java
@@ -7,10 +7,10 @@ import org.apache.commons.io.FilenameUtils;
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.tika.Tika;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
@@ -22,6 +22,8 @@ import java.util.concurrent.TimeUnit;
 
 @Service
 public class DownloadService {
+
+    private final Tika tika = new Tika();
 
     private CloseableHttpClient httpClient = HttpClientBuilder.create()
             .setConnectionTimeToLive(30, TimeUnit.SECONDS)
@@ -42,9 +44,11 @@ public class DownloadService {
                 if (extractedFilename != null) {
                     filename = extractedFilename;
                 }
+
                 type = AssetType.findByFileExtension(FilenameUtils.getExtension(filename));
                 if (type == null) {
-                    throw new DownloadError(AssetErrorCodes.INVALID_CONTENT_TYPE);
+                    // store at first as jpeg later it will get checked by tika during store
+                    type = AssetType.JPEG;
                 }
                 tempFile = File.createTempFile("asset", type.getFileExtension());
                 FileOutputStream outputStream = new FileOutputStream(tempFile);
@@ -53,7 +57,7 @@ public class DownloadService {
             } else {
                 throw new DownloadError(AssetErrorCodes.NOT_DOWNLOADABLE);
             }
-        } catch (IOException|URISyntaxException e) {
+        } catch (IOException | URISyntaxException e) {
             throw new DownloadError(AssetErrorCodes.NOT_DOWNLOADABLE);
         }
         return new TempDownload(tempFile, filename, type);
