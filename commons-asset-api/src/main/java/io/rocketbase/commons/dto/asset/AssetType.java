8b28aac2ab6421813af2a135c1b259e2a09d87ab
Marten <marten@rocketbase.io>
2019-07-30 22:55:34 +0200

    added powerpoint as asset-type

--- commons-asset-api/src/main/java/io/rocketbase/commons/dto/asset/AssetType.java
+++ commons-asset-api/src/main/java/io/rocketbase/commons/dto/asset/AssetType.java
@@ -17,7 +17,9 @@ public enum AssetType {
     XLS("application/msexcel"),
     XLSX("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"),
     DOC("application/msword"),
-    DOCX("application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+    DOCX("application/vnd.openxmlformats-officedocument.wordprocessingml.document"),
+    PPT("application/vnd.ms-powerpoint"),
+    PPTX("application/vnd.openxmlformats-officedocument.presentationml.presentation");
 
     @Getter
     private String contentType;
