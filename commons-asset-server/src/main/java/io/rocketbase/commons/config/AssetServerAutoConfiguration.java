08176409820772f252f17b30ce4f597680f10daa
Marten <marten@rocketbase.io>
2021-04-20 09:39:26 +0200

    fix: added missing conditions and missing AnalyseController

--- commons-asset-server/src/main/java/io/rocketbase/commons/config/AssetServerAutoConfiguration.java
+++ commons-asset-server/src/main/java/io/rocketbase/commons/config/AssetServerAutoConfiguration.java
@@ -3,6 +3,7 @@ package io.rocketbase.commons.config;
 import io.rocketbase.commons.controller.*;
 import io.rocketbase.commons.controller.exceptionhandler.*;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -23,38 +24,50 @@ public class AssetServerAutoConfiguration implements Serializable {
 
     @Bean
     @ConditionalOnMissingBean
+    @ConditionalOnExpression(value = "${asset.api.analyse:true}")
+    public AssetAnalyseController assetAnalyseController() {
+        return new AssetAnalyseController();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
     public AssetBaseController assetBaseController() {
         return new AssetBaseController();
     }
 
     @Bean
     @ConditionalOnMissingBean
+    @ConditionalOnExpression(value = "${asset.api.batch:true}")
     public AssetBatchController assetBatchController() {
         return new AssetBatchController();
     }
 
     @Bean
     @ConditionalOnMissingBean
+    @ConditionalOnExpression(value = "${asset.api.copy:true}")
+    public AssetCopyController assetCopyController() {
+        return new AssetCopyController();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    @ConditionalOnExpression(value = "${asset.api.delete:true}")
     public AssetDeleteController assetDeleteController() {
         return new AssetDeleteController();
     }
 
     @Bean
     @ConditionalOnMissingBean
+    @ConditionalOnExpression(value = "${asset.api.download:true}")
     public AssetDownloadController assetDownloadController() {
         return new AssetDownloadController();
     }
 
     @Bean
     @ConditionalOnMissingBean
+    @ConditionalOnExpression(value = "${asset.api.preview:true}")
     public AssetPreviewController assetPreviewController() {
         return new AssetPreviewController(assetApiProperties);
-    }
-
-    @Bean
-    @ConditionalOnMissingBean
-    public AssetCopyController assetCopyController() {
-        return new AssetCopyController();
     }
 
     // -------------------------------------------------------
